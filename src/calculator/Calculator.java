c2d97230d2da35592f0abacc968ee17775c82073
Jeevan Biradar <jeevanbiradar668@gmail.com>
2025-11-15 19:16:35 +0530

    THIS IS INITIAL COMMIT

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public interface Calculator {
+	
+	
+	public int add(int a,int b);
+	public int sub(int a,int b);
+	public int mul(int a,int b);
+	public double div(double a,double b);
+	
+	
+
+}
