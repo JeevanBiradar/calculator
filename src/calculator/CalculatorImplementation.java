b00606053d5b44adeaae6e40774e17ed75486003
Jeevan Biradar <jeevanbiradar668@gmail.com>
2025-11-17 21:06:45 +0530

    IMPLEMENT CALCULATOR CLASS

---
+++ src/calculator/CalculatorImplementation.java
@@ -0,0 +1,29 @@
+package calculator;
+
+public  class CalculatorImplementation implements Calculator {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double div(double a, double b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
