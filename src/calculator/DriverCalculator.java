0b2fbe01fc9e86bc4251eb3a4d88e70caf778edb
Jeevan Biradar <jeevanbiradar668@gmail.com>
2025-11-18 19:18:30 +0530

    Driver class add

---
+++ src/calculator/DriverCalculator.java
@@ -0,0 +1,53 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class DriverCalculator {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//use this object to call the methods
+		CalculatorImplementation c1 = new CalculatorImplementation();
+		
+		System.out.println("Enter a choice");
+		int choice = sc.nextInt();
+		
+		System.out.println("Enter a value:");
+		int a=sc.nextInt();
+		System.out.println("Enter a value:");
+		int b=sc.nextInt();
+		
+		
+		switch(choice)
+		{
+		
+		case 1:
+		{
+			//addition implementation
+		}
+		 
+		case 2:
+		{
+			//Subtraction implementation
+		}
+		
+		case 3:
+		{
+			//multiplication implementation
+		}
+		
+		case 4:
+		{
+			//Division implementation
+		}
+		
+		default:
+			System.out.println("Enter a valid choice ");
+		   
+		
+		}
+	}
+
+	}
